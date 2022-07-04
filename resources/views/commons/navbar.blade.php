2f445626bbaff1abcf18dea5c7478ba50bec6767
tech.user <tech.user@techacademy.jp>
2022-07-04 07:12:02 +0000

    トップページの実装

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,20 @@
+<header class="mb-4">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        {{-- トップページへのリンク --}}
+        <a class="navbar-brand" href="/">Chrome History Editor</a>
+
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                {{-- ログインページへのリンク --}}
+                <li class="nav-item"><a href="#" class="nav-link">ログイン</a></li>
+                {{-- ユーザ登録ページへのリンク --}}
+                <li class="nav-item"><a href="#" class="nav-link">新規登録</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
